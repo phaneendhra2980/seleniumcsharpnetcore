caca4663804b6a6408e812659ca7a9588fe0cd7f
phaneendhra2980 <phaneendhra@gmail.com>
2021-04-22 10:23:41 +0100

    initial commit
    initial commit

---
+++ Firstproject/Config/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelLTSelenium
+{
+    public static class Constants
+    {
+        internal static string Username = "Your User Name";
+        internal static string AccessKey = "Your Access Key";
+
+        //internal static boolean tunnel = Environment.GetEnvironmentVariable("TUNNEL");
+        internal static string seleniumPort = Environment.GetEnvironmentVariable("SELENIUM_PORT");
+        internal static string build = "C# Sample App";
+        internal static string seleniumHost = Environment.GetEnvironmentVariable("SELENIUM_HOST");
+
+    }
+}
---
+++ Firstproject/ParallelLT.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ParallelLTSelenium
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class ParallelLTTests
+    {
+        ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+        private String browser;
+        private String version;
+        private String os;
+
+        private static IEnumerable<TestCaseData> AddBrowserConfs()
+        {
+            yield return new TestCaseData("chrome", "89.0", "Windows 10");
+            yield return new TestCaseData("internet explorer", "11.0", "Windows 10");
+            yield return new TestCaseData("Safari", "11.0", "macOS High Sierra");
+            yield return new TestCaseData("MicrosoftEdge", "18.0", "Windows 10");
+        }
+
+        [Test,TestCaseSource("AddBrowserConfs")]
+        public void DuckDuckGo_TestCase_Demo(String browser, String version, String os)
+        {
+            String username = "user-name";
+            String accesskey = "access-key";
+            String gridURL = "@hub.lambdatest.com/wd/hub";
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
+            capabilities.SetCapability("user", "phaneendhra");
+            capabilities.SetCapability("accessKey", "3l4yDbGGvzzGON9SHnJTzExgaakER55bAmpR9JT4bRbcsppNpK");
+            capabilities.SetCapability("browserName", browser);
+            capabilities.SetCapability("version", version);
+            capabilities.SetCapability("platform", os);
+
+            driver.Value = new RemoteWebDriver(new Uri("https://" + username + ":" + accesskey + gridURL), capabilities, TimeSpan.FromSeconds(600));
+
+            System.Threading.Thread.Sleep(2000);
+
+            driver.Value.Url = "https://www.duckduckgo.com";
+
+            IWebElement element = driver.Value.FindElement(By.XPath("//*[@id='search_form_input_homepage']"));
+
+            element.SendKeys("LambdaTest");
+
+            /* Submit the Search */
+            element.Submit();
+
+            /* Perform wait to check the output */
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            bool passed = TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Passed;
+            try
+            {
+                // Logs the result to Lambdatest
+                ((IJavaScriptExecutor)driver.Value).ExecuteScript("lambda-status=" + (passed ? "passed" : "failed"));
+            }
+            finally
+            {
+                // Terminates the remote webdriver session
+                driver.Value.Quit();
+            }
+        }
+    }
+}
